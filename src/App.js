09b03642153f4a51a45c337da04d3cf48849a5bf
Pedro Camacho <pedroalmeidacamacho@gmail.com>
2024-08-26 00:18:55 -0300

    adicionando cores rgba e interatividade nas cores

--- src/App.js
+++ src/App.js
@@ -5,43 +5,43 @@ import Time from './componentes/Time';
 import Rodape from './componentes/Rodape'
 
 function App() {
-  const times = [
+  const [times, setTimes] = useState([
     {
       nome: 'programação',
-      corPrimaria: '#57C278',
-      corSecundaria: '#D9F7E9'
+      
+      cor: '#D9F7E9'
     },
     {
       nome: 'Front-End',
-      corPrimaria: '#82CFFA',
-      corSecundaria: '#E8F8FF'
+      
+      cor: '#E8F8FF'
     },
     {
       nome: 'Data Science',
-      corPrimaria: '#A6D157',
-      corSecundaria: '#F0F8E2'
+      
+      cor: '#F0F8E2'
     },
     {
       nome: 'Devops',
-      corPrimaria: '#E06B69',
-      corSecundaria: '#FDE7E8'
+      
+      cor: '#FDE7E8'
     },
     {
       nome: 'UX e Design',
-      corPrimaria: '#DB6EBF',
-      corSecundaria: '#FAE9F5'
+      
+      cor: '#FAE9F5'
     },
     {
       nome: 'Mobile',
-      corPrimaria: '#FFBA05',
-      corSecundaria: '#FFF5D9'
+      
+      cor: '#FFF5D9'
     },
     {
       nome: 'Inovação e Gestão',
-      corPrimaria: '#FF8A29',
-      corSecundaria: '#FFEEDF'
+      
+      cor: '#FFEEDF'
     }
-  ]
+  ])
 
   const inicial = [
     {
@@ -192,21 +192,18 @@ function App() {
   
   const [colaboradores, setColaboradores]=useState(inicial)
 
+  function mudarCorTime(cor, nome){
+    setTimes(times.map(time=>{
+      if(time.nome===nome){
+          time.cor=cor
+      }
+      return time
+    }))
+  }
   function deletarColaborador(){
     console.log("deletando colaborador")
   }
-  /*
   
-  */
- /*
-      {times.map(time => <Time 
-        key={time.nome} 
-        nome={time.nome} 
-        corPrimaria={time.corPrimaria} 
-        corSecundaria={time.corSecundaria}
-        colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)}
-        />)}
-  */
   const aoNovoColaboradorAdicionado = (colaborador)=>{
     console.log(colaborador)
     setColaboradores([...colaboradores,colaborador])
@@ -220,13 +217,14 @@ function App() {
         aoCadastrar={colaborador=>[aoNovoColaboradorAdicionado(colaborador),setColaboradores([...colaboradores,colaborador])]}
       />     
       <section className="times">
-        <h1>Minha organização</h1>
+        
         {times.map((time, indice) => 
           <Time 
             key={indice} 
             time={time} 
             colaboradores={colaboradores.filter(colaborador => colaborador.time === time.nome)} 
             aoDeletar={deletarColaborador}
+            mudarCor={mudarCorTime}
           />
         )}
       </section>
--- src/componentes/Colaborador/index.js
+++ src/componentes/Colaborador/index.js
@@ -4,7 +4,7 @@ import './Colaborador.css'
 const Colaborador = ({colaborador, corDeFundo, aoDeletar}) =>{
     return (
         <div className='colaborador'>
-            <IoMdCloseCircle size={25}className='deletar' onClick={aoDeletar}>deletar</IoMdCloseCircle>
+            <IoMdCloseCircle size={25} className='deletar' onClick={aoDeletar}>deletar</IoMdCloseCircle>
             <header className='cabecalho' style={{backgroundColor: corDeFundo}}>
                 <img src={colaborador.imagem} alt={colaborador.nome}/>
             </header>
